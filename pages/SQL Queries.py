2a211d3284caa9efec1ad1f669a92be4f3572082
irfanhameedofficial-png <irfanhameed.official@gmail.com>
2025-09-11 20:06:32 +0530

    SQL Queries answered for few questions

--- pages/SQL Queries.py
+++ pages/SQL Queries.py
@@ -8,7 +8,7 @@ option = st.selectbox(
     "Choose the type of SQL query to run:",
     [
         "Q1: Indian players - full details",
-        "Q2: Recent matches (last 30 days)",
+        "Q2: Recent matches",
         "Q3: Top 10 ODI run scorers",
         "Q4: Venues with 50,000+ capacity",
         "Q5: Team wins count",
@@ -50,8 +50,6 @@ if option == "Q1: Indian players - full details":
         df = pd.DataFrame(players, columns=[desc[0] for desc in cursor.description])
         st.dataframe(df,hide_index=True)
 
-    
-
 elif option == "Q2: Recent matches":
     st.subheader("Recent matches")
     st.write("This query fetches the recent matches played.")
@@ -59,7 +57,6 @@ elif option == "Q2: Recent matches":
     if btn:
         st.code("""
         select series_name,match_desc,teams,venue,city,match_date from recent_matches order by match_date desc;
-
         """, language='sql')
     btn2 = st.button("Execute Query")
     if btn2:
@@ -82,7 +79,6 @@ elif option == "Q3: Top 10 ODI run scorers":
         players = cursor.fetchall()
         df = pd.DataFrame(players, columns=[desc[0] for desc in cursor.description])
         st.dataframe(df,hide_index=True)
-
 
 elif option == "Q4: Venues with 50,000+ capacity":
     st.subheader("Venues with 50,000+ capacity")
@@ -120,11 +116,11 @@ elif option == "Q6: Player count by role":
     btn = st.button("View Query")
     if btn:
         st.code("""
-        select role,count(role) from indian_players group by role;
+        select role,count(role) from players group by role;
         """, language='sql')
     btn2 = st.button("Execute Query")
     if btn2:
-        cursor.execute("select role,count(role) from indian_players group by role")
+        cursor.execute("select role,count(role) from players group by role")
         players = cursor.fetchall()
         df = pd.DataFrame(players, columns=[desc[0] for desc in cursor.description])
         st.dataframe(df,hide_index=True)
@@ -195,9 +191,34 @@ elif option == "Q11: Player format comparison":
     btn = st.button("View Query")
     if btn:
         st.code("""
-        SELECT p.name, a.format, a.runs, a.runs/a.matches as avg FROM players p join players_details a on p.id=a.player_id WHERE format IN ('Test', 'ODI', 'T20') and runs>50;
+        SELECT 
+    p.name,
+    MAX(CASE WHEN a.format = 'Test' THEN a.average END) AS test_avg,
+    MAX(CASE WHEN a.format = 'ODI'  THEN a.average END) AS odi_avg,
+    MAX(CASE WHEN a.format = 'T20'  THEN a.average END) AS t20_avg
+FROM players p
+JOIN players_details a 
+    ON p.id = a.player_id
+WHERE a.format IN ('Test', 'ODI', 'T20')
+GROUP BY p.name;
         """, language='sql')
-    st.table()
+    btn2 = st.button("Execute Query")
+    if btn2:
+        cursor.execute("""
+SELECT 
+    p.name,
+    MAX(CASE WHEN a.format = 'Test' THEN a.average END) AS test_avg,
+    MAX(CASE WHEN a.format = 'ODI'  THEN a.average END) AS odi_avg,
+    MAX(CASE WHEN a.format = 'T20'  THEN a.average END) AS t20_avg
+FROM players p
+JOIN players_details a 
+    ON p.id = a.player_id
+WHERE a.format IN ('Test', 'ODI', 'T20')
+GROUP BY p.name
+        """)
+        players = cursor.fetchall()
+        df = pd.DataFrame(players, columns=[desc[0] for desc in cursor.description])
+        st.dataframe(df,hide_index=True)
 
 elif option == "Q12: Team home vs away wins":
     st.subheader("Team home vs away wins")
@@ -205,11 +226,26 @@ elif option == "Q12: Team home vs away wins":
     btn = st.button("View Query")
     if btn:
         st.code("""
-        SELECT * FROM players WHERE country = 'India';
+        SELECT m.winner AS team,CASE WHEN v.country = m.winner THEN 'Home' ELSE 'Away' END AS match_location,COUNT(*) AS wins FROM matches m JOIN venue v ON m.ground = v.venue AND m.city = v.city WHERE m.winner IS NOT NULL GROUP BY m.winner, match_location ORDER BY m.winner, match_location;
         """, language='sql')
     btn2 = st.button("Execute Query")
     if btn2:
-        cursor.execute("SELECT p.name, a.format, a.runs, a.runs/a.matches as avg FROM players p join players_details a on p.id=a.player_id WHERE format IN ('Test', 'ODI', 'T20') and runs>50")
+        cursor.execute("""
+SELECT 
+    m.winner AS team,
+    CASE 
+        WHEN v.country = m.winner THEN 'Home'
+        ELSE 'Away'
+    END AS match_location,
+    COUNT(*) AS wins
+FROM matches m
+JOIN venue v 
+    ON m.ground = v.venue 
+   AND m.city = v.city
+WHERE m.winner IS NOT NULL
+GROUP BY m.winner, match_location
+ORDER BY m.winner, match_location
+        """)
         players = cursor.fetchall()
         df = pd.DataFrame(players, columns=[desc[0] for desc in cursor.description])
         st.dataframe(df,hide_index=True)
@@ -220,9 +256,14 @@ elif option == "Q13: 100+ run partnerships (consecutive batsmen)":
     btn = st.button("View Query")
     if btn:
         st.code("""
-        SELECT * FROM players WHERE country = 'India';
+        SELECT batsman1, batsman2, match_id, series_name, match_desc, innings_id, partnership_runs FROM partnerships WHERE partnership_runs >= 100 ORDER BY partnership_runs DESC;
         """, language='sql')
-    st.table()
+    btn2 = st.button("Execute Query")
+    if btn2:
+        cursor.execute("SELECT batsman1, batsman2, match_id, series_name, match_desc, innings_id, partnership_runs FROM partnerships WHERE partnership_runs >= 100 ORDER BY partnership_runs DESC")
+        players = cursor.fetchall()
+        df = pd.DataFrame(players, columns=[desc[0] for desc in cursor.description])
+        st.dataframe(df,hide_index=True)
 
 elif option == "Q14: Bowler venue performance":
     st.subheader("Bowler venue performance")
@@ -240,9 +281,24 @@ elif option == "Q15: Player performance in close matches":
     btn = st.button("View Query")
     if btn:
         st.code("""
-        SELECT * FROM players WHERE country = 'India';
+        select b.player_name, avg(b.runs) as average_runs, count(b.match_id) as close_matches, count(b.match_id) as wins
+from batsmen_scores b join matches m
+on b.match_id = m.match_id
+group by b.player_name 
+having avg(runs) > 0
+order by average_runs desc;
         """, language='sql')
-    st.table()
+    btn2 = st.button("Execute Query")
+    if btn2:
+        cursor.execute("""select b.player_name, avg(b.runs) as average_runs, count(b.match_id) as close_matches, count(b.match_id) as wins
+from batsmen_scores b join matches m
+on b.match_id = m.match_id
+group by b.player_name 
+having avg(runs) > 0
+order by average_runs desc""")
+        players = cursor.fetchall()
+        df = pd.DataFrame(players, columns=[desc[0] for desc in cursor.description])
+        st.dataframe(df,hide_index=True)
 
 elif option == "Q16: Batting trends by year (since 2020)":
     st.subheader("Batting trends by year (since 2020)")
@@ -260,9 +316,28 @@ elif option == "Q17: Toss win impact analysis":
     btn = st.button("View Query")
     if btn:
         st.code("""
-        SELECT * FROM players WHERE country = 'India';
+        select toss_descision, count(*) as total_matches,
+    SUM(CASE WHEN toss = winner THEN 1 ELSE 0 END) as toss_winner_wins,
+    ROUND(
+        100.0 * SUM(CASE WHEN toss = winner THEN 1 ELSE 0 END) / COUNT(*),
+        2
+    ) as win_percentage
+from matches group by toss_descision;
         """, language='sql')
-    st.table()
+    btn2 = st.button("Execute Query")
+    if btn2:
+        cursor.execute("""
+select toss_descision, count(*) as total_matches,
+    SUM(CASE WHEN toss = winner THEN 1 ELSE 0 END) as toss_winner_wins,
+    ROUND(
+        100.0 * SUM(CASE WHEN toss = winner THEN 1 ELSE 0 END) / COUNT(*),
+        2
+    ) as win_percentage
+from matches group by toss_descision
+        """)
+        players = cursor.fetchall()
+        df = pd.DataFrame(players, columns=[desc[0] for desc in cursor.description])
+        st.dataframe(df,hide_index=True)
 
 elif option == "Q18: Most economical bowlers (ODI/T20)":
     st.subheader("Most economical bowlers (ODI/T20)")
@@ -270,9 +345,14 @@ elif option == "Q18: Most economical bowlers (ODI/T20)":
     btn = st.button("View Query")
     if btn:
         st.code("""
-        SELECT * FROM players WHERE country = 'India';
+        SELECT p.name, a.format, a.wickets, a.economy FROM players p join players_details a on p.id=a.player_id WHERE (format IN ('ODI', 'T20') and a.innings>10 and wickets>0) order by a.economy asc;
         """, language='sql')
-    st.table()
+    btn2 = st.button("Execute Query")
+    if btn2:
+        cursor.execute("SELECT p.name, a.format, a.wickets, a.economy FROM players p join players_details a on p.id=a.player_id WHERE (format IN ('ODI', 'T20') and a.innings>10 and wickets>0) order by a.economy asc")
+        players = cursor.fetchall()
+        df = pd.DataFrame(players, columns=[desc[0] for desc in cursor.description])
+        st.dataframe(df,hide_index=True)
 
 elif option == "Q19: Consistent batsmen (since 2022)":
     st.subheader("Consistent batsmen (since 2022)")
@@ -290,9 +370,41 @@ elif option == "Q20: Player match count & averages by format":
     btn = st.button("View Query")
     if btn:
         st.code("""
-        SELECT * FROM players WHERE country = 'India';
+        SELECT 
+    p.name,
+    MAX(CASE WHEN a.format = 'Test' THEN a.matches END) AS test_matches,
+    MAX(CASE WHEN a.format = 'Test' THEN a.average END) AS test_avg,
+    MAX(CASE WHEN a.format = 'ODI'  THEN a.matches END) AS odi_matches,
+    MAX(CASE WHEN a.format = 'ODI'  THEN a.average END) AS odi_avg,
+    MAX(CASE WHEN a.format = 'T20'  THEN a.matches END) AS t20_matches,
+    MAX(CASE WHEN a.format = 'T20'  THEN a.average END) AS t20_avg
+FROM players p
+JOIN players_details a 
+    ON p.id = a.player_id
+WHERE a.matches > 20
+  AND a.format IN ('Test', 'ODI', 'T20')
+GROUP BY p.name;
         """, language='sql')
-    st.table()
+    btn2 = st.button("Execute Query")
+    if btn2:
+        cursor.execute("""SELECT 
+    p.name,
+    MAX(CASE WHEN a.format = 'Test' THEN a.matches END) AS test_matches,
+    MAX(CASE WHEN a.format = 'Test' THEN a.average END) AS test_avg,
+    MAX(CASE WHEN a.format = 'ODI'  THEN a.matches END) AS odi_matches,
+    MAX(CASE WHEN a.format = 'ODI'  THEN a.average END) AS odi_avg,
+    MAX(CASE WHEN a.format = 'T20'  THEN a.matches END) AS t20_matches,
+    MAX(CASE WHEN a.format = 'T20'  THEN a.average END) AS t20_avg
+FROM players p
+JOIN players_details a 
+    ON p.id = a.player_id
+WHERE a.matches > 20
+  AND a.format IN ('Test', 'ODI', 'T20')
+GROUP BY p.name""")
+        players = cursor.fetchall()
+        df = pd.DataFrame(players, columns=[desc[0] for desc in cursor.description])
+        st.dataframe(df,hide_index=True)
+        #Can use dataframe to combine the queries
 
 elif option == "Q21: Player performance ranking":
     st.subheader("Player performance ranking")
@@ -300,9 +412,36 @@ elif option == "Q21: Player performance ranking":
     btn = st.button("View Query")
     if btn:
         st.code("""
-        SELECT * FROM players WHERE country = 'India';
+        select p.name, a.format,
+    ((a.runs * 0.1) + (a.average * 0.5) + (a.strike_rate * 0.3)) AS batting_points,
+    ((a.wickets * 2) + ((50 - a.average) * 0.5) + ((6 - a.economy) * 2)) AS bowling_points,
+    (((a.runs * 0.1) + (a.average * 0.5) + (a.strike_rate * 0.3)) +
+     ((a.wickets * 2) + ((50 - a.average) * 0.5) + ((6 - a.economy) * 2))) AS total_points,
+    DENSE_RANK() OVER (
+        PARTITION BY a.format
+        order by
+            (((a.runs * 0.1) + (a.average * 0.5) + (a.strike_rate * 0.3)) +
+             ((a.wickets * 2) + ((50 - a.average) * 0.5) + ((6 - a.economy) * 2))) desc
+    ) as format_based_rank
+from players p join players_details a on p.id = a.player_id where a.format IN ('Test','ODI','T20');
         """, language='sql')
-    st.table()
+    btn2 = st.button("Execute Query")
+    if btn2:
+        cursor.execute("""select p.name, a.format,
+    ((a.runs * 0.1) + (a.average * 0.5) + (a.strike_rate * 0.3)) AS batting_points,
+    ((a.wickets * 2) + ((50 - a.average) * 0.5) + ((6 - a.economy) * 2)) AS bowling_points,
+    (((a.runs * 0.1) + (a.average * 0.5) + (a.strike_rate * 0.3)) +
+     ((a.wickets * 2) + ((50 - a.average) * 0.5) + ((6 - a.economy) * 2))) AS total_points,
+    DENSE_RANK() OVER (
+        PARTITION BY a.format
+        order by
+            (((a.runs * 0.1) + (a.average * 0.5) + (a.strike_rate * 0.3)) +
+             ((a.wickets * 2) + ((50 - a.average) * 0.5) + ((6 - a.economy) * 2))) desc
+    ) as format_based_rank
+from players p join players_details a on p.id = a.player_id where a.format IN ('Test','ODI','T20')""")
+        players = cursor.fetchall()
+        df = pd.DataFrame(players, columns=[desc[0] for desc in cursor.description])
+        st.dataframe(df,hide_index=True)
 
 elif option == "Q22: Head-to-head team analysis":
     st.subheader("Head-to-head team analysis")
@@ -330,9 +469,43 @@ elif option == "Q24: Best batting partnerships":
     btn = st.button("View Query")
     if btn:
         st.code("""
-        SELECT * FROM players WHERE country = 'India';
+        SELECT 
+    LEAST(batsman1, batsman2) AS player1,
+    GREATEST(batsman1, batsman2) AS player2,
+    COUNT(*) AS total_partnerships,
+    ROUND(AVG(partnership_runs), 2) AS avg_runs,
+    SUM(CASE WHEN partnership_runs > 50 THEN 1 ELSE 0 END) AS fifty_plus_partnerships,
+    MAX(partnership_runs) AS highest_partnership,
+    ROUND(
+        100.0 * SUM(CASE WHEN partnership_runs > 50 THEN 1 ELSE 0 END) / COUNT(*),
+        2
+    ) AS success_rate
+FROM partnerships
+GROUP BY LEAST(batsman1, batsman2), GREATEST(batsman1, batsman2)
+HAVING COUNT(*) >= 5
+ORDER BY success_rate DESC;
         """, language='sql')
-    st.table()
+    btn2 = st.button("Execute Query")
+    if btn2:
+        cursor.execute("""
+                       SELECT 
+    LEAST(batsman1, batsman2) AS player1,
+    GREATEST(batsman1, batsman2) AS player2,
+    COUNT(*) AS total_partnerships,
+    ROUND(AVG(partnership_runs), 2) AS avg_runs,
+    SUM(CASE WHEN partnership_runs > 50 THEN 1 ELSE 0 END) AS fifty_plus_partnerships,
+    MAX(partnership_runs) AS highest_partnership,
+    ROUND(
+        100.0 * SUM(CASE WHEN partnership_runs > 50 THEN 1 ELSE 0 END) / COUNT(*),
+        2
+    ) AS success_rate
+FROM partnerships
+GROUP BY LEAST(batsman1, batsman2), GREATEST(batsman1, batsman2)
+HAVING COUNT(*) >= 5
+ORDER BY success_rate DESC""")
+        players = cursor.fetchall()
+        df = pd.DataFrame(players, columns=[desc[0] for desc in cursor.description])
+        st.dataframe(df,hide_index=True)
 
 elif option == "Q25: Player performance time-series":
     st.subheader("Player performance time-series")
@@ -345,4 +518,3 @@ elif option == "Q25: Player performance time-series":
     st.table()
 
 
-    
